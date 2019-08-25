61e04f57d8b79780004c6bf800a34e42cce3cf18
Tommy <tsuwunrut@carecloud.com>
2019-08-25 19:18:35 -0400

    cleanup

--- src/root/App.jsx
+++ src/root/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Content from '../content';
+import Content from '../content/Content.container';
 
 const App = _ => (
   <Router>
