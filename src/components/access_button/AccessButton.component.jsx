c0cb954d1faabd85e8c778852c80fdc659c10160
Tommy <tsuwunrut@carecloud.com>
2019-08-25 19:15:43 -0400

    reduced renders

--- src/components/access_button/AccessButton.component.jsx
+++ src/components/access_button/AccessButton.component.jsx
@@ -1,15 +1,12 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link, NavLink, withRouter } from 'react-router-dom';
 import { auth } from '../../firebase';
-import { ProfileContext } from '../../context';
 import { isLoggedIn } from '../../helpers';
 
 const AccessButton = _ => {
-  const { setProfile } = useContext(ProfileContext);
   const handleLogout = async _ => {
     await auth.signOut();
     sessionStorage.removeItem('Auth');
-    setProfile({});
   };
   return isLoggedIn() ? (
     <Link to="/" onClick={handleLogout}>
--- src/content/index.js
+++ src/content/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { ProfileContextProvider, ModalContextProvider } from '../context';
-import { Home, Login, MyProfile, Register, Error, Profiles, SelectedProfile } from '../pages';
+import { Home, Login, MyProfile, Register, Error as ErrorPage, Profiles, SelectedProfile } from '../pages';
 import { NavBar, AuthRoute } from '../components';
 
 const Content = _ => (
   <div className="container">
+    <NavBar />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/error" component={ErrorPage} />
     <ProfileContextProvider>
-      <NavBar />
       <ModalContextProvider>
-        <Route exact path="/" component={Home} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/error" component={Error} />
-        <AuthRoute exact path="/my-profile" component={MyProfile} />
-        <AuthRoute exact path="/profiles" component={Profiles} />
-        <AuthRoute exact path="/selected-profile/:profileId" component={SelectedProfile} />
       </ModalContextProvider>
+      <AuthRoute exact path="/my-profile" component={MyProfile} />
+      <AuthRoute exact path="/profiles" component={Profiles} />
+      <AuthRoute exact path="/selected-profile/:profileId" component={SelectedProfile} />
     </ProfileContextProvider>
   </div>
 );
