d2befa024ed096cdd94ccd454efd404f23cf9e20
tommy <stommy49@yahoo.com>
2019-09-15 18:55:46 -0400

    naming fix

--- src/context/Modal.Context.jsx
+++
@@ -1,8 +0,0 @@
-import React, { createContext, useState } from 'react';
-
-export const ModalContext = createContext({});
-
-export const ModalContextProvider = ({ children }) => {
-  const [modal, setModal] = useState({});
-  return <ModalContext.Provider value={{ modal, setModal }}>{children}</ModalContext.Provider>;
-};
--- src/context/index.js
+++
@@ -1,2 +0,0 @@
-export * from './Global.context';
-export * from './Modal.context';
